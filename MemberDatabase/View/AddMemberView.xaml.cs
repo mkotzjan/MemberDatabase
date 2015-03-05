c4df677fc877e2dc3bf96df9b6a7cc17d74f19f6
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-03-05 16:50:57 +0100

    First version of AddMemberView

--- MemberDatabase/View/AddMemberView.xaml.cs
+++ MemberDatabase/View/AddMemberView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace MemberDatabase.ViewModel
+namespace MemberDatabase.View
 {
     /// <summary>
     /// Interaction logic for AddMemberView.xaml
