8c1bd4a461bb8c5ca110614f2f974ca7d12ea0eb
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-03-07 20:43:03 +0100

    first try on button binding with command

--- MemberDatabase/ViewModel/DataGridViewModel.cs
+++ MemberDatabase/ViewModel/DataGridViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MemberDatabase.ViewModel
 {
@@ -14,6 +15,7 @@ namespace MemberDatabase.ViewModel
         public List<Member> members { get; set; }
         public string anniversary { get; set; }
         public string birthday { get; set; }
+        
         private Database db;
         private Anniversary an;
 
@@ -29,6 +31,20 @@ namespace MemberDatabase.ViewModel
             an = new Anniversary(members);
             birthday = an.nextBirthday();
             anniversary = an.nextAnniversary();
+        }
+
+
+        private ICommand _loadCommand;
+
+        public ICommand loadDatabase {
+            get
+            {
+                if (_loadCommand == null)
+                {
+                    _loadCommand = new RelayCommand(param => this.loadDataBase());
+                }
+                return _loadCommand;
+            }
         }
     }
 }
