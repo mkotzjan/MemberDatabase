067379ab1f9312669f19fcc02fe6ff93823c2573
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-03-17 20:52:07 +0100

    Add dateitem class

---
+++ MemberDatabase/Model/DateItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberDatabase.Model
+{
+    public class DateItem : INotifyPropertyChanged
+    {
+    }
+}
+
