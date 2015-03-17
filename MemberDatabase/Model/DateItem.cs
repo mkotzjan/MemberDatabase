b1441b19f35f2be49cf5c6a06f9134882572e265
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-03-17 20:52:20 +0100

    add properties

--- MemberDatabase/Model/DateItem.cs
+++ MemberDatabase/Model/DateItem.cs
@@ -9,6 +9,54 @@ namespace MemberDatabase.Model
 {
     public class DateItem : INotifyPropertyChanged
     {
+        private DateTime? dateTime;
+        private string dateInfo;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaisePropertyChanged(string prop)
+        {
+            if (this.PropertyChanged != null) { this.PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+
+        public DateTime? date
+        {
+            get
+            {
+                return dateTime;
+            }
+
+            set
+            {
+                if (value != dateTime)
+                {
+                    dateTime = value;
+                    RaisePropertyChanged("date");
+                }
+            }
+        }
+
+        public string information
+        {
+            get
+            {
+                return dateInfo;
+            }
+
+            set
+            {
+                if (value != dateInfo)
+                {
+                    dateInfo = value;
+                    RaisePropertyChanged("information");
+                }
+            }
+        }
+
+        public DateItem()
+        {
+            date = null;
+            information = string.Empty;
+        }
     }
 }
-
