e1dfd695036b456434f243788cc7b4f57cf2ff48
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-03-20 19:15:54 +0100

    Changed TesFramework to NUnit

---
+++ MemberDatabase/UnitTests/TestMember.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MemberDatabase.UnitTests
+{
+    [TestFixture]
+    public class TestMember
+    {
+        [Test]
+        public void TestInitialize()
+        {
+
+        }
+    }
+}
