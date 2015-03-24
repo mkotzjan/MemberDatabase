5ca40e408b42b87eeb897c42cb605395b05c00fa
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-03-24 15:50:48 +0100

    Add converter

--- MemberDatabase/Model/GroupingConverter.cs
+++ MemberDatabase/Model/GroupingConverter.cs
@@ -7,8 +7,33 @@ using System.Windows.Data;
 
 namespace MemberDatabase.Model
 {
+    [ValueConversion(typeof(Boolean), typeof(Int32))]
     class GroupingConverter : IValueConverter
     {
+        public object ConvertBool(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool active = (bool)value;
+            if (active)
+            {
+                return "Aktiv";
+            }
+            else
+            {
+                return "Inaktiv";
+            }
+        }
 
+        public object ConvertÍnt(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int group = (int)value;
+            if (group == 0)
+            {
+                return "Erwachsene";
+            }
+            else
+            {
+                return "Kinder";
+            }
+        }
     }
 }
